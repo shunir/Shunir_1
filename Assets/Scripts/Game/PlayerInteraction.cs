25e8d771ed43088309980e757e2d8c4c2420e57f
shunir <48938315+shunir@users.noreply.github.com>
2025-07-28 13:50:11 +0900

    fix

--- Assets/Scripts/Game/PlayerInteraction.cs
+++ Assets/Scripts/Game/PlayerInteraction.cs
@@ -5,45 +5,56 @@ public class PlayerInteraction : MonoBehaviour
 {
     private bool canInteract = false;
 
-    // Mini Game 오브젝트와 충돌 시
-    private void OnTriggerEnter2D(Collider2D other) 
+   
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("minigame"))
+        
+        if (other.CompareTag("MiniGame")) 
         {
             canInteract = true;
             Debug.Log("미니게임 오브젝트 근처에 있습니다. F키를 눌러 상호작용하세요.");
         }
-
-        else if ( other.CompareTag("WallTrigger")); 
+        
+        else if (other.CompareTag("WallTrigger")) 
+        {
+            Debug.Log("WallTrigger에 닿았습니다.");
+            
+        }
     }
 
-    // Mini Game 오브젝트에서 벗어날 시
-    private void OnTriggerExit2D(Collider2D other) // 3D인 경우 OnTriggerExit로 변경
+    
+    private void OnTriggerExit2D(Collider2D other)
     {
-        if (other.CompareTag("minigam"))
+       
+        if (other.CompareTag("MiniGame")) 
         {
             canInteract = false;
             Debug.Log("미니게임 오브젝트에서 벗어났습니다.");
         }
     }
 
+  
+    
+ 
+       
     private void OnCollisionEnter2D(Collision2D collision)
     {
-    
-        if (collision.gameObject.CompareTag("Wall")) 
+        
+        if (collision.gameObject.CompareTag("Wall"))
         {
             Debug.Log("벽에 부딪혔습니다. 게임은 계속됩니다.");
         }
-        
     }
+
+
 
     void Update()
     {
-        // F키를 누르고 상호작용 가능한 상태일 때
+        
         if (canInteract && Input.GetKeyDown(KeyCode.F))
         {
-            Debug.Log("F키 입력 감지! 플로피버드 씬으로 이동합니다.");
-            SceneManager.LoadScene("FlappyPlane_Project"); // 플로피버드 씬 이름으로 변경
+            Debug.Log("F키 입력 감지! 플래피버드 씬으로 이동합니다.");
+            SceneManager.LoadScene("FlappyPlane_Project"); 
         }
     }
 }
